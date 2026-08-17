4551da3767ac9c1a2a74d67c4db4a900873434e4
Luan Brancalione <luancarlosbrancalione@gmail.com>
2026-08-17 19:20:46 -0300

    refactor: separar dados de teste

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
+import { users } from '../test-data/users';
 test('login com usuário válido', async ({ page }) => {
 await page.goto('https://www.saucedemo.com/');
-await page.locator('[data-test="username"]').fill('standard_user');
-await page.locator('[data-test="password"]').fill('secret_sauce');
+await page.locator('[data-test="username"]').fill(users.standard.username);
+await page.locator('[data-test="password"]').fill(users.standard.password);
 await page.locator('[data-test="login-button"]').click();
 await expect(page.locator('[data-test="title"]')).toHaveText('Products');
 });
@@ -21,5 +22,6 @@ await page.locator('[data-test="login-button"]').click();
 await expect(page.locator('[data-test="title"]')).toHaveText('Products');
 await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
 await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
-await expect(page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')).toBeVisible();
+console.log('Remove visível:', await page.locator('[data-test="remove-sauce-labs-backpack"]').isVisible());
+console.log('Add visível:', await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').isVisible());
 });
